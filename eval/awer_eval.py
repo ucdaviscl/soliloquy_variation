38ef0e8fa1c8432f82bc04a3ad8da17239758494
Kenji Sagae <sagae@dss04949-lin-d.ou.ad3.ucdavis.edu>
2018-10-29 18:36:57 -0700

    improved eval scripts, easier to run on other corpora

--- eval/awer_eval.py
+++ eval/awer_eval.py
@@ -3,30 +3,53 @@
 import os 
 import sys
 
+# AWER EVAL
+#
+# Script to run artificial word error rate evaluation
+# of language models for limited-domain user speech
+# input.
+#
+# usage: ./awer_eval.py data_path top_n dev_fname fake_fname tmp_dir
+
+if len(sys.argv) != 6:
+  print('usage: ./awer_eval.py data_path top_n dev_fname fake_fname tmp_dir',
+        file=sys.stderr)
+  sys.exit(1)
+
 # training data for the language model must be divided
 # into files named 000.txt, 001.txt, 002.txt, ...
 # with each file containing 64 sentences.
 # (this number is arbitrary.)
-#
+
 # data_path is the path to the directory containing 
 # these files.
-
 data_path = sys.argv[1]
 
 # top_n is the number of files to consider
-
 top_n = int(sys.argv[2])
+
+# dev_fname is the file with heldout data
+dev_fname = sys.argv[3]
+
+# fake_fname is the file with fake utterances
+fake_fname = sys.argv[4]
+
+# tmp_dir is the name of a temporary directory
+tmp_dir = sys.argv[5]
 
 flist = []
 txtlist = []
+flen = []
 
 # now create files with increasing amount of
 # training data
 
 for i in range(top_n):
-  outfname = "000-%03d" % i + '.txt'
+  outfname = tmp_dir + "/000-%03d" % i + '.txt'
   infname = data_path + '/' + "%03d" % i + '.txt'
+  nlines = sum(1 for line in open(infname, 'r', encoding='utf-8'))
   flist.append(infname)
+  flen.append(nlines)
   argstr = ' '.join(flist)
   os.system('cat ' + argstr + ' > ' + outfname)
   txtlist.append(outfname)
@@ -34,11 +57,13 @@ for i in range(top_n):
 # create language models using increasing amount of
 # training data, and check artificial word error rate
 
+print('\n------------> baseline <-------------\n')
+
 for fname in txtlist:
   #print(fname)
   #os.system('wc -l ' + fname)
   os.system('./mklm.sh ' + fname + ' ' + fname + '.lm')
-  os.system('cat dev.txt | ./awer.py -v tr.unigrams -f ' + fname + '.lm')
+  os.system('cat ' + dev_fname + ' | ./awer.py -u tr.unigrams -f ' + fname + '.lm')
   
 print('\n------------> with automatic variety <-------------\n')
 
@@ -46,9 +71,8 @@ print('\n------------> with automatic variety <-------------\n')
 # training data, and check artificial word error rate
 
 for i in range(top_n):
-  nstr = str((i+1)*64)
-  os.system('head -' + nstr + ' 000-009.top1 > head.tmp')
-  os.system('cat ' + txtlist[i] + ' head.tmp > lm.tmp')
-  os.system('./mklm.sh lm.tmp lm2.tmp')
-  os.system('cat dev.txt | ./awer.py -v tr.unigrams -f lm2.tmp')
-
+  nstr = str((i+1)*flen[i])
+  os.system('head -' + nstr + ' ' + fake_fname + ' > ' + tmp_dir + '/head.tmp')
+  os.system('cat ' + txtlist[i] + ' ' + tmp_dir + '/head.tmp > ' + tmp_dir + '/lm.tmp')
+  os.system('./mklm.sh ' + tmp_dir + '/lm.tmp ' + tmp_dir + '/lm2.tmp')
+  os.system('cat ' + dev_fname + ' | ./awer.py -u tr.unigrams -f ' + tmp_dir + '/lm2.tmp')
--- eval/oov_eval.py
+++ eval/oov_eval.py
@@ -7,26 +7,36 @@ import sys
 # into files named 000.txt, 001.txt, 002.txt, ...
 # with each file containing 64 sentences.
 # (this number is arbitrary.)
-#
+
 # data_path is the path to the directory containing 
 # these files.
-
 data_path = sys.argv[1]
 
 # top_n is the number of files to consider
-
 top_n = int(sys.argv[2])
+
+# dev_fname is the file with heldout data
+dev_fname = sys.argv[3]
+
+# fake_fname is the file with fake utterances
+fake_fname = sys.argv[4]
+
+# tmp_dir is the name of a temporary directory
+tmp_dir = sys.argv[5]
 
 flist = []
 txtlist = []
+flen = []
 
 # now create files with increasing amount of
 # training data
 
 for i in range(top_n):
-  outfname = "000-%03d" % i + '.txt'
+  outfname = tmp_dir + "/000-%03d" % i + '.txt'
   infname = data_path + '/' + "%03d" % i + '.txt'
+  nlines = sum(1 for line in open(infname, 'r', encoding='utf-8'))
   flist.append(infname)
+  flen.append(nlines)
   argstr = ' '.join(flist)
   os.system('cat ' + argstr + ' > ' + outfname)
   txtlist.append(outfname)
@@ -35,7 +45,7 @@ for i in range(top_n):
 # training data, and check artificial word error rate
 
 for fname in txtlist:
-  os.system('./oov.py ' + fname + ' dev.txt')
+  os.system('./oov.py ' + fname + ' ' + dev_fname)
   
 print('\n------------> with automatic variety <-------------\n')
 
@@ -43,8 +53,8 @@ print('\n------------> with automatic variety <-------------\n')
 # training data, and check artificial word error rate
 
 for i in range(top_n):
-  nstr = str((i+1)*64)
-  os.system('head -' + nstr + ' 000-009.top1 > head.tmp')
-  os.system('cat ' + txtlist[i] + ' head.tmp > lm.tmp')
-  os.system('./oov.py lm.tmp dev.txt')
+  nstr = str((i+1)*flen[i])
+  os.system('head -' + nstr + ' ' + fake_fname + ' > ' + tmp_dir + '/head.tmp')
+  os.system('cat ' + txtlist[i] + ' ' + tmp_dir + '/head.tmp > ' + tmp_dir + '/lm.tmp')
+  os.system('./oov.py ' + tmp_dir + '/lm.tmp ' + dev_fname)
 
