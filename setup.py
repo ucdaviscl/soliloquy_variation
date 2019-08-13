2e3f2db8b20c7fa43a4e11fb1d14446db5fb4e15
Matt Reese <mwreese@ucdavis.edu>
2019-08-13 15:32:36 -0700

    Created setup file for packaging

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='SoliloquyVariation',
+    version='0.1.0',
+    packages=['soliloquy_variation'],
+    url='https://github.com/ucdaviscl/soliloquy_variation',
+    license='LICENSE',
+    description='Generation of lexical and sentence-level variety using word vectors, sequence to sequence models, and transducers',
+    long_description=open('README.md').read(),
+)
